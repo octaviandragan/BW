b9a3e470ab46fae08db95c3a886c53e243e5b724
octaviandragan <dragan.tavi@yahoo.com>
2025-04-04 22:28:07 +0200

    Cleanup of forgotten TC and update last-run snapshot

--- tests/userProfile.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from './fixtures';
-import { UserProfilePage } from '../pages/UserProfile.page';
-
-test('should accept valid input', async ({ page }) => {
-    const userProfile = new UserProfilePage(page);
-    await userProfile.fillUserProfile({ firstName: 'John' });
-});
